b2b8070fc7403df567afadb840148ccb78c9fe0e
Lukas Borke <lukas.borke@web.de>
2019-12-10 16:22:10 +0100

    update notebooks_keras_buch/keras_bsp_5.2.py

--- notebooks_keras_buch/keras_bsp_5.2.py
+++ notebooks_keras_buch/keras_bsp_5.2.py
@@ -103,7 +103,7 @@ print('total test cat images:', len(os.listdir(test_cats_dir)))
 print('total test dog images:', len(os.listdir(test_dogs_dir)))
 
 
-## Training
+## Training, ohne vorherigen Teil lauffähig
 from keras import layers
 from keras import models
 
@@ -128,5 +128,84 @@ from keras import optimizers
 model.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+
+import os
+
+base_dir = 'T:\\temp_data\\cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+# https://keras.io/preprocessing/image/
+
+train_generator = train_datagen.flow_from_directory(
+        # This is the target directory
+        train_dir,
+        # All images will be resized to 150x150
+        target_size=(150, 150),
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+
+# check
+for data_batch, labels_batch in train_generator:
+    print('data batch shape:', data_batch.shape)
+    print('labels batch shape:', labels_batch.shape)
+    break
+
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=30,
+      validation_data=validation_generator,
+      validation_steps=50)
+
+
+# ca. 35 sek pro Epoche auf Ryzen
+# Epoch 30/30 100/100 [==============================] - 36s 356ms/step - loss: 0.0312 - acc: 0.9900 - val_loss: 1.0706 - val_acc: 0.7370
+
+model.save('T:\\temp_data\\cats_and_dogs_small\\cats_and_dogs_small_1.h5')
+
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
 
 
