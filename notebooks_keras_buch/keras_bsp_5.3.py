0b2002664b231d159253ab2ae058976b173c7d2b
Lukas Borke <lukas.borke@web.de>
2019-12-11 18:12:25 +0100

    update notebooks_keras_buch/keras_bsp_5.3.py

--- notebooks_keras_buch/keras_bsp_5.3.py
+++ notebooks_keras_buch/keras_bsp_5.3.py
@@ -10,6 +10,20 @@ keras.__version__
 
 from keras.applications import VGG16
 
+from keras import models
+from keras import layers
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
+import os
+
+base_dir = 'T:\\temp_data\\cats_and_dogs_small'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+
 conv_base = VGG16(weights='imagenet',
                   include_top=False,
                   input_shape=(150, 150, 3))
@@ -18,20 +32,12 @@ conv_base = VGG16(weights='imagenet',
 conv_base.summary()
 
 
-import os
+## simple feature extraction model
+
 import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
-
-
-base_dir = 'T:\\temp_data\\cats_and_dogs_small'
-
-train_dir = os.path.join(base_dir, 'train')
-validation_dir = os.path.join(base_dir, 'validation')
-test_dir = os.path.join(base_dir, 'test')
 
 datagen = ImageDataGenerator(rescale=1./255)
 batch_size = 20
-
 
 def extract_features(directory, sample_count):
     features = np.zeros(shape=(sample_count, 4, 4, 512))
@@ -59,8 +65,99 @@ train_features, train_labels = extract_features(train_dir, 2000)
 validation_features, validation_labels = extract_features(validation_dir, 1000)
 test_features, test_labels = extract_features(test_dir, 1000)
 
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
 
 
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
 
+model.summary()
+
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+
+history = model.fit(train_features, train_labels,
+                    epochs=30,
+                    batch_size=20,
+                    validation_data=(validation_features, validation_labels))
+
+
+## data augmentation model
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+print('This is the number of trainable weights before freezing the conv base:', len(model.trainable_weights))
+
+conv_base.trainable = False
+
+print('This is the number of trainable weights before freezing the conv base:', len(model.trainable_weights))
+
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+
+train_generator = train_datagen.flow_from_directory(
+        # This is the target directory
+        train_dir,
+        # All images will be resized to 150x150
+        target_size=(150, 150),
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=2e-5),
+              metrics=['acc'])
+
+
+# https://keras.io/models/sequential/
+# verbose: Integer. 0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      # epochs=30,
+      epochs=15,
+      validation_data=validation_generator,
+      validation_steps=50,
+      verbose=1)
+
+
+# ca. 150 sek pro Epoche auf Ryzen
+# Epoch 2/2 - 149s - loss: 0.4621 - acc: 0.8065 - val_loss: 0.3580 - val_acc: 0.8740
+
+model.save('T:\\temp_data\\cats_and_dogs_small\\cats_and_dogs_small_3_test.h5')
 
 
